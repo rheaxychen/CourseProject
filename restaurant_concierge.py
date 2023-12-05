60d922e977514fed1299af38375c3b0e6a9b7a74
xchenc <74441880+xchenc@users.noreply.github.com>
2023-12-04 23:41:33 -0500

    new ver

--- restaurant_concierge.py
+++ restaurant_concierge.py
@@ -5,10 +5,19 @@ from math import radians, sin, cos, sqrt, atan2
 from BM25 import BM25Ranker  
 import time
 from collections import defaultdict
+import nltk
+import ast
+import tkinter as tk
+from tkinter import scrolledtext
+import re
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
+from difflib import get_close_matches
+
 
 class RestaurantConcierge:
-
-    def __init__(self, distance = 80,ip_address='', filepath = 'yelp_dataset'):
+# 80km = 50 mile
+    def __init__(self, distance = 82,ip_address='', filepath = 'yelp_dataset'):
         self.ip_address = ip_address
         self.filepath = filepath
         self.filepath_business = self.filepath + '/yelp_academic_dataset_business.json'
@@ -119,17 +128,31 @@ class RestaurantConcierge:
             
             print(f'Total count for match: {c}')
 
+            ##########################
+            ########
+
+            business_expect_min =10
+            business_exxpect_max = 20
             # Adjust self.distance based on the count
-            if c <= 15:
+            if c <= business_expect_min and self.distance < 150:
                 # If count is 0, increase self.distance by a certain amount
                 self.distance += 0.1
                 print(f'No businesses found within the current distance. Increasing distance to {self.distance}.')
                 return self.get_business_within_distance(data, user_location)
-            elif c > 150:
-                # If count is more than 150, decrease self.distance by a certain amount
+            
+            elif c > business_exxpect_max and self.distance < 150:
+                # If count is more than business_exxpect_max, decrease self.distance by a certain amount
                 self.distance -= 0.1
-                print(f'More than 150 businesses found within the current distance. Decreasing distance to {self.distance}.')
+                print(f'More than {business_exxpect_max} businesses found within the current distance. Decreasing distance to {self.distance}.')
                 return self.get_business_within_distance(data, user_location)
+            
+            elif c < 10 and self.distance >= 150:
+                # the case if business do not have 10 within distance
+                print(f'That is all business found within {self.distance} km')
+                return businesses_within_distance
+            elif c <= 0 and self.distance >= 150:
+                print(f'No business found near by.')
+                return
             else:
                 # If count is within the desired range, update self.business_withinLocation and return the result
                 self.business_withinLocation = businesses_within_distance
@@ -177,10 +200,34 @@ class RestaurantConcierge:
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {str(e)}")
             return None
+        
+    def find_similar_words(self,keyword, text):
 
+        similar_words = None
+        # Get close matches
+        if keyword:
+            # Get all words from WordNet
+            all_words = set(word for synset in wordnet.all_synsets() for word in synset.lemma_names())
+            words = word_tokenize(text.lower())
+            similar_words = get_close_matches(keyword.lower(), all_words)
+            
+            # Filter out words that are already in the text
+            similar_words = [word for word in similar_words if word not in words]
+        
+        return similar_words
+
+    ###TODO: return result base on the query input(taco should return business related to taco)
     # get rank function
-    def get_rank(self, queries, weight_bm25=0.7, weight_stars=0.5, weight_useful=0.33, weight_funny=0.05, weight_cool=0.05):
+    def get_rank(self, keyword, weight_bm25=0.7, weight_stars=0.5, weight_useful=0.33, weight_funny=0.05, weight_cool=0.05):
         try:
+            # download package incase first time run
+            nltk.download('wordnet')
+
+            queries = ['Excellent', 'Outstanding', 'Superb', 'Exceptional', 'Amazing',
+                            'Delicious', 'Fantastic', 'Wonderful', 'Terrific', 'Great',
+                            'Impressive', 'Outstanding', 'Marvelous', 'Exceptional', 'Top-notch',
+                            'High-quality', 'Delectable', 'Tasty', 'Yummy', 'Superlative'
+                        ]
             if self.review_data is None:
                 print("Review data is not loaded. Call 'get_review_data' first.")
                 return
@@ -221,15 +268,36 @@ class RestaurantConcierge:
             for business_id in combined_scores:
                 combined_scores[business_id]['bm25_score'] /= max_bm25_score
 
-            # Calculate the final combined score using weights
+            # Calculate the relevance percentage for each result based on the keyword
             for business_id in combined_scores:
-                combined_scores[business_id]['combined_score'] = (
-                    weight_bm25 * combined_scores[business_id]['bm25_score'] +
-                    weight_stars * combined_scores[business_id]['stars'] +
-                    weight_useful * combined_scores[business_id]['useful'] +
-                    weight_funny * combined_scores[business_id]['funny'] +
-                    weight_cool * combined_scores[business_id]['cool']
-                )
+                review_text = next((review['text'] for review in user_reviews_with_features if review['business_id'] == business_id), '')  
+                if keyword is None or len(keyword) <= 1:
+                    combined_scores[business_id]['relevance_percentage'] = 0
+                    combined_scores[business_id]['combined_score'] = (
+                            weight_bm25 * combined_scores[business_id]['bm25_score'] +
+                            weight_stars * combined_scores[business_id]['stars'] +
+                            weight_useful * combined_scores[business_id]['useful'] +
+                            weight_funny * combined_scores[business_id]['funny'] +
+                            weight_cool * combined_scores[business_id]['cool'])
+                    
+                    
+                else:
+                    # Use regular expressions to find occurrences of the keyword and similar words
+                    keyword_occurrences = len(re.findall(r'\b' + re.escape(keyword.lower()) + r'\b', review_text.lower()))
+                    
+                    # Use NLTK to find similar words
+                    similar_words_occurrences = len(self.find_similar_words(keyword, review_text))
+
+                    total_words = len(word_tokenize(review_text))
+                    relevance_percentage = ((keyword_occurrences + similar_words_occurrences) / total_words) * 100
+                    combined_scores[business_id]['relevance_percentage'] = relevance_percentage
+                    combined_scores[business_id]['combined_score'] = (
+                            weight_bm25 * combined_scores[business_id]['bm25_score'] +
+                            weight_stars * combined_scores[business_id]['stars'] +
+                            weight_useful * combined_scores[business_id]['useful'] +
+                            weight_funny * combined_scores[business_id]['funny'] +
+                            weight_cool * combined_scores[business_id]['cool'])
+
 
             # Sort businesses based on the combined scores
             ranked_businesses = sorted(
@@ -237,41 +305,86 @@ class RestaurantConcierge:
                 key=lambda x: x[1]['combined_score'],
                 reverse=True
             )
+            keyword_percentage = defaultdict(lambda: 0.0)
+            for index, review in enumerate(user_reviews_with_features):
+                if keyword.lower() in review['text'].lower():
+                    business_id = review['business_id']
+                    keyword_percentage[business_id] += 1.0
+            total_reviews_for_business = len([review for review in user_reviews_with_features if review['business_id'] == business_id])
+            max_keyword_count = max(keyword_percentage.values())
+            for business_id in keyword_percentage:
+                keyword_percentage[business_id] /= max_keyword_count
+                keyword_percentage[business_id] /= total_reviews_for_business
+                keyword_percentage[business_id] *= 100  # Convert to percentage
+
 
             # Extract the relevant information for the result
             ranked_businesses_info = []
             for business_id, score in ranked_businesses:
                 business_info = self.fetch_business_name(business_id)
+
                 if business_info:
                     is_open_status = 'open' if business_info.get('is_open', 0) == 1 else 'closed'
-                combined_address = ' '.join(filter(None, [
+                    combined_address = ' '.join(filter(None, [
                         business_info.get('address', ''),
                         business_info.get('city', ''),
                         business_info.get('state', ''),
                         business_info.get('postal_code', '')
                     ]))
-                
-                # Construct the location string
-                latitude = business_info.get('latitude')
-                longitude = business_info.get('longitude')
-                location_str = f'latitude: {latitude}, longitude: {longitude}' if latitude is not None and longitude is not None else ''
 
-                ranked_business = {
-                    'name': business_info.get('name', ''),
-                    'business_id': business_id,
-                    'combined_score': score['combined_score'],
-                    'address': combined_address,
-                    'location': location_str,
-                    'is_open': is_open_status,
-                    'categories': business_info.get('categories'),
-                    'stars': business_info.get('stars'),
-                    'hours': business_info.get('hours', {})
-                    # Add other business info fields as needed
-                }
-                ranked_businesses_info.append(ranked_business)
+                    # Construct the location string
+                    latitude = business_info.get('latitude')
+                    longitude = business_info.get('longitude')
+                    location_str = (
+                        f'latitude: {latitude}, longitude: {longitude}'
+                        if latitude is not None and longitude is not None
+                        else ''
+                    )
+
+                    # Extract the 'GoodForMeal' attribute from the 'attributes' dictionary
+                    arrt_Ambience = None
+                    type_arrt = None
+                    all_attribute = business_info.get('attributes', {})
+                    if all_attribute:
+                        arrt_Ambience = all_attribute.get('Ambience', {})
+                        type_arrt = all_attribute.get('GoodForMeal', {})
+
+                    # Get the relevance percentage for the current business
+                    if keyword is None or len(keyword) < 1:
+                        relevance_percentage = 0
+                    else:
+                        relevance_percentage = keyword_percentage.get(business_id, 0.0)
+
+                    # Check if 'combined_score' key is present in the score dictionary
+                    combined_score = score.get('combined_score', 0.0)
+                    combined_score = combined_score/1000
+
+                    ranked_business = {
+                        'name': business_info.get('name', ''),
+                        'business_id': business_id,
+                        'combined_score': combined_score,
+                        'address': combined_address,
+                        'location': location_str,
+                        'is_open': is_open_status,
+                        'categories': business_info.get('categories'),
+                        'stars': business_info.get('stars'),
+                        'hours': business_info.get('hours', {}),
+                        'details': all_attribute,
+                        'Ambience': arrt_Ambience,
+                        'type': type_arrt,
+                        'relevance_percentage': relevance_percentage,  # Add relevance percentage to the dictionary
+                        # Add other business info fields as needed
+                    }
+
+                    ranked_businesses_info.append(ranked_business)
+                
+                else:
+                    print(f"Skipping business with ID {business_id} because business_info is None.")
             return ranked_businesses_info
         except Exception as e:
             print(f'get_rank error out: {e}')
+
+
 
     # fetch the business name and address by business_id
     def fetch_business_name(self, business_id):
@@ -295,24 +408,43 @@ class RestaurantConcierge:
             return None
 
     # main process for getting everything
-    def run_main(self, addr = None):
-        if addr is None:
-            concierge.get_business_within_distance(self.business_data,concierge.get_address_by_ip())
-        else:
-            concierge.get_business_within_distance(self.business_data,concierge.get_lat_long_by_addr(addr))
-        start_time = time.time()
-        concierge.get_review_data()
-        end_time = time.time()
-        queries = ["delicious food", "best restaurants near me","awesome"]
-        ranked_businesses = concierge.get_rank(queries)
-        for res in ranked_businesses[:10]:
-            print(res)
-            print('\n')
-        # Calculate the elapsed time
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print('done')
-        print(f"Time taken: {elapsed_time} seconds for main process")
+    def run_main(self, keyword = "", addr=None):
+        try:
+            if addr is None:
+                location = self.get_address_by_ip()
+                if location is None:
+                    print("Unable to determine current location. Exiting.")
+                    return
+                self.get_business_within_distance(self.business_data, location)
+            else:
+                location = self.get_lat_long_by_addr(addr)
+                if location is None:
+                    print(f"Unable to find location for address: {addr}. Exiting.")
+                    return
+                self.get_business_within_distance(self.business_data, location)
+            
+            start_time = time.time()
+            self.get_review_data()
+            end_time = time.time()
+            ranked_businesses = self.get_rank(keyword)
+            if ranked_businesses is None:
+                print('Cannot find the result in your area')
+                print('\n', 'Program Done')
+            else:
+                for res in ranked_businesses[:10]:
+                    print(res)
+                    print('\n')
+            # Calculate the elapsed time
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            
+            print('done')
+            print(f"Time taken: {elapsed_time} seconds for main process")
+            return ranked_businesses[:10]
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
 
 
 if __name__ == "__main__":
@@ -325,7 +457,6 @@ if __name__ == "__main__":
         option = int(input("Enter the number corresponding to your choice: "))
         concierge = RestaurantConcierge()
         business_data = concierge.read_yelp_data()
-
         if option == 1:
             concierge.run_main()
             
